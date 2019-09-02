f4b02ae33c83ac669c31d474a5d7b31d3ff9d397
Liam Sutton <devliam90@gmail.com>
2019-09-02 12:21:34 +0100

    add all nav items to page

--- js/index.js
+++ js/index.js
@@ -83,3 +83,14 @@ contactParagraph.forEach((item, i) => {
   i += 1;
   item.id = 'contactParagraph' + i;
 })
+
+// Add nav items
+navItems[0].textContent = siteContent.nav['nav-item-1'];
+navItems[1].textContent = siteContent.nav['nav-item-2'];
+navItems[2].textContent = siteContent.nav['nav-item-3'];
+navItems[3].textContent = siteContent.nav['nav-item-4'];
+navItems[4].textContent = siteContent.nav['nav-item-5'];
+navItems[5].textContent = siteContent.nav['nav-item-6'];
+
+
+
